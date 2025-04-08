77d99ce062d13994942e98f88d292039aaea06c9
tring-bharath <bharath.a@tringapps.com>
2025-04-08 07:48:45 +0530

    file change

--- Main-task/index.js
+++ Main-task/index.js
@@ -54,10 +54,10 @@ function showForm(formId) {
     var img = document.getElementById("image");
     if (formId === "signInForm") {
         document.querySelector(".btn1").classList.add("active");
-        img.src = "https://s3-alpha-sig.figma.com/img/604e/1b5c/7d3c27a774af4e69a6b43fde874c130d?Expires=1739750400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=ROFbOXQrGEygKeIWT077YAUhtyPJ~HBQFbDf4KAM1tJv6YSeMzJxNxChFBU~49tPVvxNMWruY169MNRJPWBtcHMG1rRxHRJFk5SKccg0-~GHDcW4L959tdJNgXUqYwKRik0JN4h4qcSk6Yqksladu2ns8r4ViJFm2qELOS44bYN-rpE-zN49B6IgYyrvt5rzqeuzwH8Trqoe46vqMsDqCkjfKdP0g7bHdZUKtkOTQnBKHHjyqdTFV3kwnBPAI-PA3EgasW97XuBpeAueFVDd6ghxnrB7n~BU5N-bXQN23dNDxh0EDyokFJc9L3nsiBoMT5gnPcdIE4s3Id-uHkpMOQ__";
+        img.src = "./signin.png";
     } else {
         document.querySelector(".btn2").classList.add("active");
-        img.src = "https://s3-alpha-sig.figma.com/img/a177/c757/e4cf6291368cc1d53989ca3678003ee0?Expires=1739750400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=j~cMqlg537QuuUWg5IO3eg0e~XPudiNjHyqhWV0Wh6HTCy1H4bMaoBRHJlqexSX00qOFkW7qe2sx9r6xdnR0suNqHmzn-hGN4pJvfbUFNqHYY3IyG-aciMZ13vRCfGq01jEuewuq-W9htGyy6toN0U9J0Oz9TKShvvL122gg3yBcCHjNzxjsEUpPb62XeFEtVv0kr9MtdYQTlKiMXY2pIG4Ek2A1r1sRl9-0-IBwSzHd6ocHiQNO2snWkuWCv7YS8Cnw7d017y03PW-AHvRThCIRxP-tz5VyVpuqbH7bS2rA2riEQdt0d7FMQOwcWXOTpzwpgTiwhlKZvgr06vhDfQ__";
+        img.src = "./signup.png";
     }
 }
 document.addEventListener("DOMContentLoaded", function () {
